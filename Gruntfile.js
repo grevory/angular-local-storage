3a4436f5afc53d1db6cc8408ebd1959ad95ed179
Eddie Monge <eddie@eddiemonge.com>
2013-09-23 14:01:30 -0700

    Add Grunt to automate testing and linting. Add Karma to run tests

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Load the grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Time the grunt tasks
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    karma: {
+      options: {
+        autowatch: false,
+        browsers: [
+          'PhantomJS'
+        ],
+        configFile: 'test/karma.conf.js',
+        reporters: [ 'dots' ],
+        singleRun: true
+      },
+      unit: {}
+    },
+    jshint: {
+      grunt: {
+        src: [ 'Gruntfile.js' ],
+        options: {
+          node: true
+        }
+      },
+      dev: {
+        src: [ 'localStorageModule.js' ],
+        options: {
+          jshintrc: '.jshintrc',
+        }
+      },
+      test: {
+        src: [ 'test/spec/**/*.js' ],
+        options: {
+          jshintrc: 'test/.jshintrc',
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('test', [
+    'karma'
+  ]);
+
+  grunt.registerTask('default', [
+    'jshint',
+    'test'
+  ]);
+};
