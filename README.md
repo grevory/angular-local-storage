9993ed43e0430f5292325b8645916b336ffb02e6
Gregory Pike <greg@gregpike.ca>
2013-12-26 23:14:15 -0500

    Updated demo and documentation

--- README.md
+++ README.md
@@ -3,20 +3,6 @@ angular-local-storage
 
 An Angular module that gives you access to the browsers local storage
 
-Remember to set your app name (settings.appPrefix) in the settings at the beginning of angular-local-storage.js:
-
-```javascript
-angular.module('LocalStorageModule').value('prefix', 'myPre');
-angular.module('testApp', ['LocalStorageModule'])
-  .config(function ($routeProvider) {
-    ....
-});
-```
-
-To do:
-- Add tests
-- Expand Readme
-
 Example use: 
 
 ```javascript
@@ -40,3 +26,8 @@ angular.module('yourModule', ['LocalStorageModule'])
 });
 ```
 Check out the full demo and documentation at http://gregpike.net/demos/angular-local-storage/demo.html
+
+To do:
+- Add tests
+- Expand Readme
+
--- demo/demo-app.js
+++ demo/demo-app.js
@@ -1,16 +1,23 @@
-var DemoCtrl = function($scope, localStorageService) {
-    
-  localStorageService.clearAll();
+angular.module('demoModule', ['LocalStorageModule'])
+.config(['localStorageServiceProvider', function(localStorageServiceProvider){
+  localStorageServiceProvider.setPrefix('demoPrefix');
+}])
+.controller('DemoCtrl', [
+  '$scope',
+  'localStorageService',
+  function($scope, localStorageService) {
+    // Start fresh
+    localStorageService.clearAll();
 
-  $scope.$watch('localStorageDemo', function(value){
-    localStorageService.add('localStorageDemo',value);
-    $scope.localStorageDemoValue = localStorageService.get('localStorageDemo');
-  });
+    $scope.$watch('localStorageDemo', function(value){
+      localStorageService.add('localStorageDemo',value);
+      $scope.localStorageDemoValue = localStorageService.get('localStorageDemo');
+    });
 
-  $scope.storageType = 'Local storage';
+    $scope.storageType = 'Local storage';
 
-  if (!localStorageService.isSupported) {
-    $scope.storageType = 'Cookie';
+    if (!localStorageService.isSupported) {
+      $scope.storageType = 'Cookie';
+    }
   }
-
-};
+]);
