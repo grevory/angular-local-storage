459456e74ac08ebf30974a902beb696cc505375a
grevory <greg@gregpike.ca>
2016-07-29 12:22:35 -0700

    Updated release to 0.3.0

--- dist/angular-local-storage.js
+++ dist/angular-local-storage.js
@@ -1,6 +1,6 @@
 /**
  * An Angular module that gives you access to the browsers local storage
- * @version v0.2.8 - 2016-07-14
+ * @version v0.3.0 - 2016-07-29
  * @link https://github.com/grevory/angular-local-storage
  * @author grevory <greg@gregpike.ca>
  * @license MIT License, http://www.opensource.org/licenses/MIT
